bbebe6ffcfcc0edf446d98d0fc3f350bda57b0fd
LauraCedam <bolivarmelolaura@gmail.com>
2025-07-16 15:56:02 -0500

    maquetados

---
+++ PruebasPHP/HolaMundo.php
@@ -0,0 +1,4 @@
+<?php
+ print "¡Hola, Mundo! <br>";
+ //echo variuos mensajes
+    echo "¡Hola, PHP!", "otro mensaje";
---
+++ PruebasPHP/Variables.php
@@ -0,0 +1,12 @@
+<?php 
+$texto = "ejemplo de string";
+print $texto . "<br>";
+
+//cambio valor
+$texto ="cambio de valor";
+echo $texto . "<br>";
+
+$nombre ="Laura";
+$apellido ="Bolivar";
+echo "Welcome $nombre $apellido <br>";
+echo "welcome" .$nombre . " 1230 " . $apellido . "<br>";
